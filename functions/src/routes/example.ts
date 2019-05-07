079aa852efd7e0ccc11481aaf80fb72aa0d82458
lyukio <lyukio@hotmail.com>
2019-05-07 19:03:11 -0300

    .

--- functions/src/routes/example.ts
+++ functions/src/routes/example.ts
@@ -4,6 +4,6 @@ import * as express from "express"
 const router = express.Router()
 router.post('/', controller.route_postJSON.bind(controller))
 router.get('/:exampleId', controller.route_getJSON.bind(controller))
-router.get('/otherExample-examples', controller.route_getOtherExampleExamplesJSON.bind(controller))
+router.get('/otherExample-examples', controller.route_getOtherExampleExamplesJSON.bind(controller))//TODO: inverter o para pegar os "otherExamples" de um "example"
 
 export const exampleRouter = router
