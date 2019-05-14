731149695f8d5803bece8e84445239e77e3ef0ff
lyukio <lyukio@hotmail.com>
2019-05-14 15:10:56 -0300

    editing token utils

--- functions/src/utils/token.ts
+++ functions/src/utils/token.ts
@@ -1,31 +1,21 @@
-//TODO: ver se é possível usar o token pelo firebase auth
+import * as jwt from "jsonwebtoken"
 
-// import * as jwt from "jsonwebtoken";
+export type TokenPayload = {
+    [key: string]: any
+    userId: string
+}
+export class Token {
+    private static secret = "Huddle!2018#"
+    static generate(payload: TokenPayload, expiresIn = "24h") {
+        let token = jwt.sign(payload, this.secret, {expiresIn})
+        return token
+    }
 
-// export type TokenPayload = {
-//     [key: string]: any;
-//     exp: number;
-//     uid: string;
-//     workspace?: string;
-// }
-// export class Token {
-//     private static secret = "Huddle!2018"; 
-//     static generate(payload: TokenPayload) {
-//         // if(payload.exp) {
-//         //
-//         // }
-//         let token = jwt.sign(payload,this.secret);
-//         return token;
-//     }
-
-//     static validate(token: string) {
-//         let decoded;
-//         try{
-//             decoded = jwt.verify(token,this.secret);
-//         } catch(err) {
-//             return null;
-//         }
-
-//         return decoded;
-//     }
-// }
+    static validate(token: string) {
+        try {
+            return jwt.verify(token, this.secret)
+        } catch(err) {
+            return null
+        }
+    }
+}
