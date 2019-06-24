168f4a11c49f34f15c828850d521b722ff954d2f
lyukio <lyukio@hotmail.com>
2019-06-24 18:15:36 -0300

    editing middlewareExample

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -2,12 +2,12 @@ import * as functions from 'firebase-functions'
 import * as express from 'express'
 
 import {routes as app} from './routes/routes'
-// import {middlewareTest} from './utils/middlewareTest'
+import {middlewareExample} from './utils/middlewareExample'
 import { crossDomain } from "./utils/crossDomain"
 
 const main = express()
 
 main.use(crossDomain)
-// main.use(middlewareTest)
+main.use(middlewareExample)
 main.use(app)
 export const api = functions.https.onRequest(main)
--- functions/src/utils/middlewareExample.ts
+++ functions/src/utils/middlewareExample.ts
@@ -1,6 +1,6 @@
 import { Request, NextFunction, Response } from "express";
 
-export const middlewareTest = (req: Request, res: Response, next: NextFunction) => {
+export const middlewareExample = (req: Request, res: Response, next: NextFunction) => {
     console.log("passed on middleware");
     next();
 }
